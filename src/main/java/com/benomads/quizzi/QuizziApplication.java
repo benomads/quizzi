73e6ba49d18629f5ea9f964defe7e0cf7e129fbd
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-22 22:46:01 +0600

    Quizzi web application/start

---
+++ src/main/java/com/benomads/quizzi/QuizziApplication.java
@@ -0,0 +1,13 @@
+package com.benomads.quizzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizziApplication.class, args);
+    }
+
+}
