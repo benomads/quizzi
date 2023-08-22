89aafd399ce5ea34592d18e555913cf23d52cfc9
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-23 00:28:18 +0600

    fix bugs in Question.class

--- src/main/java/com/benomads/quizzi/Question.java
+++ src/main/java/com/benomads/quizzi/Question.java
@@ -13,13 +13,14 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String questionTitle;
     private String option1;
     private String option2;
     private String option3;
     private String option4;
-    private String levelOfComplexity;
-    private String rightAnswer;
+    private String difficulty_level;
+    private String correctAnswer;
     private String category;
 
 
