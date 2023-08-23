eb00c3ba7f2965f2bc10d60c7e93ff1494ae69f6
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-23 20:16:39 +0600

    new QuizController

---
+++ src/main/java/com/benomads/quizzi/controller/QuizController.java
@@ -0,0 +1,10 @@
+package com.benomads.quizzi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "quiz")
+public class QuizController {
+
+}
