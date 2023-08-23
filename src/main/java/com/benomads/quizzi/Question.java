5a16db73bc57a38e0117022866d47f1441fd07e7
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-23 19:26:13 +0600

    in Question.class @GeneratedValue(strategy = GenerationType.SEQUENCE) ==> @GeneratedValue(strategy = GenerationType.IDENTITY)
    added addQuestion(POST request) method in QuestionController and QuestionService

--- src/main/java/com/benomads/quizzi/Question.java
+++ src/main/java/com/benomads/quizzi/Question.java
@@ -11,7 +11,7 @@ import lombok.Data;
 public class Question {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String questionTitle;
