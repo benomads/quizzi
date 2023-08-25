450c2ad91e1e226213c3c970fff9596c3591e9d5
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-26 00:18:15 +0600

    fixing some bags

--- src/main/java/com/benomads/quizzi/service/QuizService.java
+++ src/main/java/com/benomads/quizzi/service/QuizService.java
@@ -2,18 +2,20 @@ package com.benomads.quizzi.service;
 
 import com.benomads.quizzi.dao.QuestionDao;
 import com.benomads.quizzi.dao.QuizDao;
+
 import com.benomads.quizzi.model.Question;
 import com.benomads.quizzi.model.QuestionWrapper;
 import com.benomads.quizzi.model.Quiz;
 import com.benomads.quizzi.model.Response;
-import jakarta.persistence.criteria.CriteriaBuilder;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
