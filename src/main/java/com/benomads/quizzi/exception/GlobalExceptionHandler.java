6ab600dee95863cd5a243fa62563f15096029ca6
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-10-10 09:22:36 +0600

    rewrote GlobalExceptionHandler.java, made script more readable and clean, but i think it would be better, if i separate question, quiz, and user

--- src/main/java/com/benomads/quizzi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/benomads/quizzi/exception/GlobalExceptionHandler.java
@@ -12,25 +12,15 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    HttpStatus httpNotFoundErrorStatus = HttpStatus.NOT_FOUND;
-    HttpStatus httpFoundErrorStatus = HttpStatus.FOUND;
+    private final HttpStatus httpNotFoundErrorStatus = HttpStatus.NOT_FOUND;
+    private final HttpStatus httpFoundErrorStatus = HttpStatus.FOUND;
 
 
     @ExceptionHandler(QuestionNotFoundException.class)
     public ResponseEntity<Object> handleQuestionNotFoundException(Exception ex, //We need to replace Exception.class
                                                                   HttpServletRequest request) {
 
-
-        ApiException apiException =
-            new ApiException(
-                    ZonedDateTime.now(ZoneId.of("UTC+6")),
-                    httpNotFoundErrorStatus.value(),
-                    httpNotFoundErrorStatus,
-                    ex.getMessage(),
-                    request.getRequestURI()
-            );
-
-
+        ApiException apiException = getApiException(ex, request, httpFoundErrorStatus);
         return new ResponseEntity<>(apiException, httpNotFoundErrorStatus);
     }
 
@@ -38,15 +28,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleQuestionAlreadyExistException(Exception ex,
                                                                       HttpServletRequest request) {
 
-        ApiException apiException =
-            new ApiException(
-                    ZonedDateTime.now(ZoneId.of("UTC+6")),
-                    httpFoundErrorStatus.value(),
-                    httpFoundErrorStatus,
-                    ex.getMessage(),
-                    request.getRequestURI()
-            );
-
+        ApiException apiException = getApiException(ex, request, httpFoundErrorStatus);
         return new ResponseEntity<>(apiException, httpFoundErrorStatus);
     }
 
@@ -55,15 +37,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleQuizNotFoundException(Exception ex,
                                                               HttpServletRequest request) {
 
-        ApiException apiException =
-            new ApiException(ZonedDateTime.now(
-                    ZoneId.of("UTC+6")),
-                    httpNotFoundErrorStatus.value(),
-                    httpNotFoundErrorStatus,
-                    ex.getMessage(),
-                    request.getRequestURI()
-            );
-
+        ApiException apiException = getApiException(ex, request, httpFoundErrorStatus);
         return new ResponseEntity<>(apiException, httpNotFoundErrorStatus);
     }
 
@@ -72,17 +46,22 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleUserNotFoundException(Exception ex, //We need to replace Exception.class
                                                                   HttpServletRequest request) {
 
+        ApiException apiException = getApiException(ex, request, httpFoundErrorStatus);
+        return new ResponseEntity<>(apiException, httpNotFoundErrorStatus);
+    }
 
-        ApiException apiException =
+
+    private static ApiException getApiException(Exception ex, HttpServletRequest request, HttpStatus httpStatus) {
+        return
             new ApiException(
                 ZonedDateTime.now(ZoneId.of("UTC+6")),
-                httpNotFoundErrorStatus.value(),
-                httpNotFoundErrorStatus,
+                httpStatus.value(),
+                httpStatus,
                 ex.getMessage(),
                 request.getRequestURI()
             );
-
-
-        return new ResponseEntity<>(apiException, httpNotFoundErrorStatus);
     }
+
+
+
 }
